4d412212921841c5d4b7502a20c7b8ee97e2391a
devstackq <upzq@mail.ru>
2020-10-18 15:48:54 +0600

    fix

--- test.go
+++
@@ -1,3 +0,0 @@
-filter -> client -> checkbox (nature) -> backend -> query sql, get all post where cat_id =? , nature ?
-
-|| get all posts, send client -> dataset, Js sort, filter by checkbox value, and show  post -> category(nature)
