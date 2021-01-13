c2f811a57da3742896c7187686a575cf3046d397
devstackq <loginpsn@mail.ru>
2021-01-13 20:19:09 +0600

    try create table

--- config/config.go
+++ config/config.go
@@ -51,53 +51,58 @@ func Init() {
 	// 	return
 	// }
 
-	post, err := db.Prepare("CREATE TABLE IF NOT EXISTS posts(id serial PRIMARY KEY, thread varchar, content varchar, creator_id integer, create_time datetime,   update_time datetime DEFAULT current_timestamp, image	bytea NOT NULL, count_like INTEGER DEFAULT 0, count_dislike INTEGER DEFAULT 0, FOREIGN KEY(creator_id) REFERENCES users(id) ON DELETE CASCADE ) ")
+	post, err := db.Prepare("CREATE TABLE IF NOT EXISTS posts(id serial PRIMARY KEY, thread varchar(255), content varchar(255), creator_id integer, create_time datetime,   update_time datetime DEFAULT current_timestamp, image	bytea NOT NULL, count_like INTEGER DEFAULT 0, count_dislike INTEGER DEFAULT 0, FOREIGN KEY(creator_id) REFERENCES users(id) ON DELETE CASCADE ); ")
 	if err != nil {
 		log.Println(err, "1")
 	}
-	postCategoryBridge, err := db.Prepare("CREATE TABLE IF NOT EXISTS post_cat_bridge(id SERIAL PRIMARY KEY, post_id INTEGER, category_id INTEGER)")
+	post.Exec()
+
+	postCategoryBridge, err := db.Prepare("CREATE TABLE IF NOT EXISTS post_cat_bridge(id SERIAL PRIMARY KEY, post_id INTEGER, category_id INTEGER);")
 	if err != nil {
 		log.Println(err, "2")
-	}
-	//postCategoryBridge, err := db.Prepare("CREATE TABLE IF NOT EXISTS post_cat_bridge(id SERIAL PRIMARY KEY, post_id INTEGER, category_id INTEGER, CONSTRAINT fk_key_pcb_cat FOREIGN KEY(category_id) REFERENCES category(id), CONSTRAINT fk_key_pcb_post FOREIGN KEY(post_id) REFERENCES posts(id) )")
-	comment, err := db.Prepare("CREATE TABLE IF NOT EXISTS comments(id SERIAL PRIMARY KEY, parent_id INTEGER DEFAULT 0, content VARCHAR, post_id INTEGER, creator_id INTEGER DEFAULT 0, toWho INTEGER DEFAULT 0, fromWho INTEGER DEFAULT 0, create_time datetime,  update_time	datetime DEFAULT CURRENT_TIMESTAMP,  count_like INTEGER DEFAULT 0, count_dislike  INTEGER DEFAULT 0, CONSTRAINT fk_key_post_comment FOREIGN KEY(post_id) REFERENCES posts(id) ON DELETE CASCADE )")
-	if err != nil {
-		log.Println(err, "3")
-	}
-
-	session, err := db.Prepare("CREATE TABLE IF NOT EXISTS session(id SERIAL PRIMARY KEY, uuid	VARCHAR, user_id INTEGER UNIQUE, cookie_time datetime)")
-	if err != nil {
-		log.Println(err, "4")
-	}
-	user, err := db.Prepare("CREATE TABLE IF NOT EXISTS users(id SERIAL NOT NULL PRIMARY KEY, full_name VARCHAR NOT NULL, email	VARCHAR NOT NULL UNIQUE, username VARCHAR NOT NULL UNIQUE, password VARCHAR, isAdmin INTEGER DEFAULT 0, age INTEGER, sex VARCHAR, created_time	datetime, last_seen datetime, city VARCHAR, image	bytea NOT NULL)")
-	if err != nil {
-		log.Println(err, "5")
-	}
-	voteState, err := db.Prepare("CREATE TABLE IF NOT EXISTS voteState(id SERIAL PRIMARY KEY,  user_id INTEGER, post_id INTEGER, comment_id INTEGER,   like_state INTEGER  DEFAULT 0, dislike_state INTEGER  DEFAULT 0, unique(post_id, user_id), FOREIGN KEY(comment_id) REFERENCES comments(id), FOREIGN KEY(post_id) REFERENCES posts(id))")
-	if err != nil {
-		log.Println(err, "6")
-	}
-	notify, err := db.Prepare("CREATE TABLE IF NOT EXISTS notify(id SERIAL PRIMARY KEY, post_id INTEGER,  current_user_id INTEGER, voteState INTEGER DEFAULT 0, created_time datetime, to_whom INTEGER, comment_id INTEGER )")
-	if err != nil {
-		log.Println(err, "7")
-	}
-
-	category, err := db.Prepare("CREATE TABLE IF NOT EXISTS  category(id SERIAL PRIMARY KEY, name VARCHAR UNIQUE, test VARCHAR)")
-	if err != nil {
-		log.Println(err)
 	}
 	pcb, err := postCategoryBridge.Exec()
 	if err != nil {
 		log.Println(err, "exec err 2")
 	}
 	fmt.Println(pcb, "pcb")
-	post.Exec()
+	//postCategoryBridge, err := db.Prepare("CREATE TABLE IF NOT EXISTS post_cat_bridge(id SERIAL PRIMARY KEY, post_id INTEGER, category_id INTEGER, CONSTRAINT fk_key_pcb_cat FOREIGN KEY(category_id) REFERENCES category(id), CONSTRAINT fk_key_pcb_post FOREIGN KEY(post_id) REFERENCES posts(id) )")
+	comment, err := db.Prepare("CREATE TABLE IF NOT EXISTS comments(id SERIAL PRIMARY KEY, parent_id INTEGER DEFAULT 0, content varchar(255), post_id INTEGER, creator_id INTEGER DEFAULT 0, toWho INTEGER DEFAULT 0, fromWho INTEGER DEFAULT 0, create_time datetime,  update_time	datetime DEFAULT CURRENT_TIMESTAMP,  count_like INTEGER DEFAULT 0, count_dislike  INTEGER DEFAULT 0, CONSTRAINT fk_key_post_comment FOREIGN KEY(post_id) REFERENCES posts(id) ON DELETE CASCADE );")
+	if err != nil {
+		log.Println(err, "3")
+	}
 	comment.Exec()
+
+	session, err := db.Prepare("CREATE TABLE IF NOT EXISTS session(id SERIAL PRIMARY KEY, uuid	varchar(255), user_id INTEGER UNIQUE, cookie_time datetime);")
+	if err != nil {
+		log.Println(err, "4")
+	}
 	session.Exec()
+
+	user, err := db.Prepare("CREATE TABLE IF NOT EXISTS users(id SERIAL NOT NULL PRIMARY KEY, full_name varchar(255) NOT NULL, email	varchar(255) NOT NULL UNIQUE, username varchar(255) NOT NULL UNIQUE, password varchar(255), isAdmin INTEGER DEFAULT 0, age INTEGER, sex varchar(255), created_time	datetime, last_seen datetime, city varchar(255), image	bytea NOT NULL);")
+	if err != nil {
+		log.Println(err, "5")
+	}
 	user.Exec()
+
+	voteState, err := db.Prepare("CREATE TABLE IF NOT EXISTS voteState(id SERIAL PRIMARY KEY,  user_id INTEGER, post_id INTEGER, comment_id INTEGER,   like_state INTEGER  DEFAULT 0, dislike_state INTEGER  DEFAULT 0, unique(post_id, user_id), FOREIGN KEY(comment_id) REFERENCES comments(id), FOREIGN KEY(post_id) REFERENCES posts(id));")
+	if err != nil {
+		log.Println(err, "6")
+	}
 	voteState.Exec()
+
+	notify, err := db.Prepare("CREATE TABLE IF NOT EXISTS notify(id SERIAL PRIMARY KEY, post_id INTEGER,  current_user_id INTEGER, voteState INTEGER DEFAULT 0, created_time datetime, to_whom INTEGER, comment_id INTEGER );")
+	if err != nil {
+		log.Println(err, "7")
+	}
 	notify.Exec()
+
+	category, err := db.Prepare("CREATE TABLE IF NOT EXISTS  category(id SERIAL PRIMARY KEY, name varchar(255) UNIQUE) );")
+	if err != nil {
+		log.Println(err)
+	}
 	category.Exec()
+
 	putCategoriesInDb()
 
 	//send packege - DB conn
