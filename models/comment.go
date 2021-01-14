d729db51b5258bafc13d2f866c204f1e4e6b3acc
devstackq <loginpsn@mail.ru>
2021-01-14 16:43:34 +0600

    try fix some bugs

--- models/comment.go
+++ models/comment.go
@@ -8,49 +8,44 @@ import (
 
 //Comment ID -> foreign key -> postID
 type Comment struct {
-	ID          int       `json:"id"`
-	Content     string    `json:"content"`
-	PostID      string    `json:"postId"`
-	UserID      int       `json:"userId"`
-	Author      string    `json:"author"`
-	Like        int       `json:"like"`
-	Dislike     int       `json:"dislike"`
-	TitlePost   string    `json:"titlePost"`
-	Time         string `json:"time"`
-	CreatedTime  time.Time    `json:"createdTime"`
-	UpdatedTime time.Time    `json:"updatedTime"`
-	ToWhom      int       `json:"toWhom"`
-	FromWhom    int       `json:"fromWhom"`
-	Replied    string       `json:"replied"`
-	ReplyID     int       `json:"replyId"`
-	Parent int `json:"parent"`
-	Children []Comment `json:"children"`
-	Edited bool `json:"edited"`
-	CommentID int `json:"cid"`
-	ParentID int `json:"parentId"`
-	RepliedContent string `json:"repliedContent"`
+	ID             int       `json:"id"`
+	Content        string    `json:"content"`
+	PostID         string    `json:"postId"`
+	UserID         int       `json:"userId"`
+	Author         string    `json:"author"`
+	Like           int       `json:"like"`
+	Dislike        int       `json:"dislike"`
+	TitlePost      string    `json:"titlePost"`
+	Time           string    `json:"time"`
+	CreatedTime    time.Time `json:"createdTime"`
+	UpdatedTime    time.Time `json:"updatedTime"`
+	ToWhom         int       `json:"toWhom"`
+	FromWhom       int       `json:"fromWhom"`
+	Replied        string    `json:"replied"`
+	ReplyID        int       `json:"replyId"`
+	Parent         int       `json:"parent"`
+	Children       []Comment `json:"children"`
+	Edited         bool      `json:"edited"`
+	CommentID      int       `json:"cid"`
+	ParentID       int       `json:"parentId"`
+	RepliedContent string    `json:"repliedContent"`
 }
 
-
 //LeaveComment for post by id
-func (c *Comment) LeaveComment() (int64) {
+func (c *Comment) LeaveComment() int64 {
 
-	commentPrepare, err := DB.Prepare(`INSERT INTO comments(content, post_id, creator_id, create_time) VALUES($1,$2,$3,$4)`)
+	var lid int64
+	err = DB.QueryRow("INSERT INTO comments(content, post_id, creator_id, create_time) VALUES($1,$2,$3,$4) RETURNING id", c.Content, c.PostID, c.UserID, time.Now()).Scan(&lid)
 	if err != nil {
 		log.Println(err)
 	}
-	commentExec, err := commentPrepare.Exec(c.Content, c.PostID, c.UserID, time.Now())
-	if err != nil {
-		log.Println(err)
-	}
-	defer commentPrepare.Close()
 
 	//commet content
 	err = DB.QueryRow("SELECT creator_id FROM posts WHERE id=$1", c.PostID).Scan(&c.ToWhom)
 	if err != nil {
 		log.Println(err)
 	}
-	lid, err := commentExec.LastInsertId()
+
 	if err != nil {
 		log.Println(err)
 	}
@@ -61,7 +56,7 @@ func (c *Comment) LeaveComment() (int64) {
 //UpdateComment func
 func (c *Comment) UpdateComment() {
 	_, err := DB.Exec("UPDATE comments SET content=$1, update_time=$2 WHERE id=$3",
-		c.Content, c.UpdatedTime, c.ID )
+		c.Content, c.UpdatedTime, c.ID)
 	if err != nil {
 		log.Println(err)
 	}
